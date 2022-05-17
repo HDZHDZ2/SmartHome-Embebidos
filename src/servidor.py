a45f015ec58923333c9fac9ddb0ed9defad5039a
AJHH <31052396+HDZHDZ2@users.noreply.github.com>
2022-05-16 20:42:21 -0500

    Update de proyecto
    Se envía todas las peticiones al servidor, y regresa resultados por GET

--- src/servidor.py
+++ src/servidor.py
@@ -1,15 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return render_template('/Simulador/casa.html')
-
-@app.route('/focos')
+@app.route('/focos',methods=["GET","POST"])
 def focos():
-    return render_template('/Focos/Focos.html')
+	return render_template('/Focos/Focos.html')
+
+@app.route('/',methods=['GET'])
+def index():
+	
+	return render_template('/Simulador/casa.html')
 
 @app.route('/camaras')
 def camaras():
@@ -17,5 +18,41 @@ def camaras():
 
 @app.route('/timbre')
 def timbre():
-    return render_template('/Timbre_Garage/Timbre_Garage.html')
+    return render_template('/Timbre/Timbre.html')
+
+@app.route('/Open', methods=["GET","POST"])
+def Open():
+	lugar=request.form.get("GarageO")
+	estado='Open'
+	return render_template('/Simulador/casa.html', lugar=lugar,estado=estado)
+
+@app.route('/Close', methods=["GET","POST"])
+def Close():
+	lugar=request.form.get("GarageC")
+	estado='Close'
+	return render_template('/Simulador/casa.html', lugar=lugar,estado=estado)
+
+@app.route('/Encender', methods=["GET","POST"])
+def Encender():
+	lugar=request.form.get("Encender")
+	estado='On'
+	return render_template('/Simulador/casa.html', lugar=lugar,estado=estado)
+
+@app.route('/Apagar', methods=["GET","POST"])
+def Apagar():
+	lugar=request.form.get("Apagar")
+	estado='Off'
+	return render_template('/Simulador/casa.html', lugar=lugar,estado=estado)
+
+@app.route('/Camara1', methods=["GET","POST"])
+def Camara1():
+	lugar=request.form.get("Camara1")
+	estado='Camara1'
+	return redirect('http://192.168.1.156:4747/video')
+
+@app.route('/Camara2', methods=["GET","POST"])
+def Camara2():
+	lugar=request.form.get("Camara2")
+	estado='Camara2'
+	return render_template('/Simulador/casa.html', lugar=lugar,estado=estado)
 
---
+++ src/static/main.js
@@ -0,0 +1,163 @@
+function Foco1()
+{
+	estado=localStorage.getItem('Foco1');
+	var imagen= document.getElementById('Foco1');
+	if(estado=='On')
+	{
+    imagen.src="static/foco.png";
+	}
+	else
+	{
+    imagen.src="static/focoapagado.png";	
+	}
+}
+
+function Foco2(status)
+{
+	estado=localStorage.getItem('Foco2');
+	var imagen= document.getElementById('Foco2');
+	if(estado=='On')
+	{
+    imagen.src="static/foco.png";
+	}
+	else
+	{
+    imagen.src="static/focoapagado.png";	
+	}
+}
+
+function Foco3(status)
+{
+	estado=localStorage.getItem('Foco3');
+	var imagen= document.getElementById('Foco3');
+	if(estado=='On')
+	{
+    imagen.src="static/foco.png";
+	}
+	else
+	{
+    imagen.src="static/focoapagado.png";	
+	}
+}
+
+function Foco4()
+{
+	estado=localStorage.getItem('Foco4');
+	var imagen= document.getElementById('Foco4');
+	if(estado=='On')
+	{
+    imagen.src="static/foco.png";
+	}
+	else
+	{
+    imagen.src="static/focoapagado.png";	
+	}
+}
+
+function Foco5()
+{
+	estado=localStorage.getItem('Foco5');
+	var imagen= document.getElementById('Foco5');
+	if(estado=='On')
+	{
+    imagen.src="static/foco.png";
+	}
+	else
+	{
+    imagen.src="static/focoapagado.png";	
+	}
+}
+
+function Foco6()
+{
+	estado=localStorage.getItem('Foco6');
+	var imagen= document.getElementById('Foco6');
+	if(estado=='On')
+	{
+    imagen.src="static/foco.png";
+	}
+	else
+	{
+    imagen.src="static/focoapagado.png";	
+	}
+}
+
+function Garage()
+{
+	estado = localStorage.getItem('garage');
+	var imagen= document.getElementById('Carro');
+	if(estado=='Open')
+	{
+    imagen.src="static/carro.png";
+	}
+	else
+	{
+    imagen.src="";
+	}
+}
+
+function Camara1()
+{
+  window.open("http://0.0.0.0:8081/Camara1.mp4", "Camara1", "width=300, height=200")
+}
+
+function Camara2()
+{
+  window.open("http://0.0.0.0:8081/Camara1.mp4", "Camara2", "width=300, height=200")  
+}
+
+function Camara3()
+{
+  window.open("http://192.168.1.232:4747/", "Camara3", "width=800, height=800")  
+}
+
+
+function Timbre()
+{
+  const audio = new Audio("static/Timbre1.mp3");
+  alert("Tocan la puerta!!!!!!");
+  audio.play();
+}
+
+function main(action,value)
+{
+	if(action=='Foco1')
+	{
+		localStorage.setItem('Foco1',value);
+		Foco1();
+	}
+	if(action=='Foco2')
+	{
+		localStorage.setItem('Foco2',value);
+		Foco2();
+	}
+	if(action=='Foco3')
+	{
+		localStorage.setItem('Foco3',value);
+		Foco3();
+	}
+	if(action=='Foco4')
+	{
+		localStorage.setItem('Foco4',value);
+		Foco4();
+	}
+	if(action=='Foco5')
+	{
+		localStorage.setItem('Foco5',value);
+		Foco5();
+	}
+	if(action=='Foco6')
+	{
+		localStorage.setItem('Foco6',value);
+		Foco6();
+	}
+
+	if(action=='Garage')
+	{
+		localStorage.setItem('garage',value);
+		Garage();
+	}
+
+}
+
+
